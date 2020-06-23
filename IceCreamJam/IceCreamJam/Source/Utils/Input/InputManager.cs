09c36a97d464eb4dfcd7a347c9ed5513e857dc16
lightningstudios126 <ethan.wong126@gmail.com>
2020-06-22 22:05:57 -0400

    oops i don't know why this is partial but it's not anymore

--- IceCreamJam/IceCreamJam/Source/Utils/Input/InputManager.cs
+++ IceCreamJam/IceCreamJam/Source/Utils/Input/InputManager.cs
@@ -2,67 +2,67 @@
 using Nez;
 
 namespace IceCreamJam {
-	static partial class InputManager {
-		/// <summary>
-		/// negative values are up, positive values are down
-		/// </summary>
-		public static readonly VirtualAxis yAxis;
-		/// <summary>
-		/// negative values are left, positive values are right
-		/// </summary>
-		public static readonly VirtualAxis xAxis;
+    static class InputManager {
+        /// <summary>
+        /// negative values are up, positive values are down
+        /// </summary>
+        public static readonly VirtualAxis yAxis;
+        /// <summary>
+        /// negative values are left, positive values are right
+        /// </summary>
+        public static readonly VirtualAxis xAxis;
 
-		/// <summary>
-		/// x: negative values are left, positive values are right<br/>
-		/// y: negative values are up, positive values are down
-		/// </summary>
-		public static readonly VirtualJoystick joystick;
+        /// <summary>
+        /// x: negative values are left, positive values are right<br/>
+        /// y: negative values are up, positive values are down
+        /// </summary>
+        public static readonly VirtualJoystick joystick;
 
-		public static readonly VirtualButton brake;
-		public static readonly VirtualButton dash;
-		public static readonly VirtualButton dispense;
+        public static readonly VirtualButton brake;
+        public static readonly VirtualButton dash;
+        public static readonly VirtualButton dispense;
 
-		public static readonly VirtualButton shoot;
-		public static readonly VirtualIntegerAxis switchWeapon;
+        public static readonly VirtualButton shoot;
+        public static readonly VirtualIntegerAxis switchWeapon;
 
-		static InputManager() {
-			xAxis = new VirtualAxis();
-			xAxis.Nodes.Add(new VirtualAxis.KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.A, Keys.D));
-			xAxis.Nodes.Add(new VirtualAxis.GamePadLeftStickX());
+        static InputManager() {
+            xAxis = new VirtualAxis();
+            xAxis.Nodes.Add(new VirtualAxis.KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.A, Keys.D));
+            xAxis.Nodes.Add(new VirtualAxis.GamePadLeftStickX());
 
-			yAxis = new VirtualAxis();
-			yAxis.Nodes.Add(new VirtualAxis.KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.W, Keys.S));
-			yAxis.Nodes.Add(new VirtualAxis.GamePadLeftStickY());
+            yAxis = new VirtualAxis();
+            yAxis.Nodes.Add(new VirtualAxis.KeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.W, Keys.S));
+            yAxis.Nodes.Add(new VirtualAxis.GamePadLeftStickY());
 
-			joystick = new VirtualJoystick(false);
-			joystick.AddKeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.A, Keys.D, Keys.W, Keys.S);
-			joystick.AddGamePadLeftStick();
+            joystick = new VirtualJoystick(false);
+            joystick.AddKeyboardKeys(VirtualInput.OverlapBehavior.TakeNewer, Keys.A, Keys.D, Keys.W, Keys.S);
+            joystick.AddGamePadLeftStick();
 
-			brake = new VirtualButton();
-			brake.AddKeyboardKey(Keys.LeftShift);
-			// TODO: add gamepad button for braking
+            brake = new VirtualButton();
+            brake.AddKeyboardKey(Keys.LeftShift);
+            // TODO: add gamepad button for braking
 
-			dash = new VirtualButton();
-			dash.AddKeyboardKey(Keys.Space);
-			// TODO: add gamepad button for dashing
+            dash = new VirtualButton();
+            dash.AddKeyboardKey(Keys.Space);
+            // TODO: add gamepad button for dashing
 
-			dispense = new VirtualButton();
-			dispense.AddKeyboardKey(Keys.F);
-			// TODO: add gamepad button for dispensing
+            dispense = new VirtualButton();
+            dispense.AddKeyboardKey(Keys.F);
+            // TODO: add gamepad button for dispensing
 
-			shoot = new VirtualButton();
-			shoot.AddMouseLeftButton();
-			shoot.AddGamePadRightTrigger(0, 0.2f);
+            shoot = new VirtualButton();
+            shoot.AddMouseLeftButton();
+            shoot.AddGamePadRightTrigger(0, 0.2f);
 
-			var prevKey = new VirtualButton(new VirtualButton.KeyboardKey(Keys.Q));
-			var nextKey = new VirtualButton(new VirtualButton.KeyboardKey(Keys.E));
-			var prevGamepad = new VirtualButton(new VirtualButton.GamePadButton(0, Buttons.X));
-			var nextGamepad = new VirtualButton(new VirtualButton.GamePadButton(0, Buttons.A));
+            var prevKey = new VirtualButton(new VirtualButton.KeyboardKey(Keys.Q));
+            var nextKey = new VirtualButton(new VirtualButton.KeyboardKey(Keys.E));
+            var prevGamepad = new VirtualButton(new VirtualButton.GamePadButton(0, Buttons.X));
+            var nextGamepad = new VirtualButton(new VirtualButton.GamePadButton(0, Buttons.A));
 
-			switchWeapon = new VirtualIntegerAxis();
-			switchWeapon.Nodes.Add(new ButtonAxis(prevKey, nextKey));
-			switchWeapon.Nodes.Add(new ScrollAxis());
-			switchWeapon.Nodes.Add(new ButtonAxis(prevGamepad, nextGamepad));
-		}
-	}
+            switchWeapon = new VirtualIntegerAxis();
+            switchWeapon.Nodes.Add(new ButtonAxis(prevKey, nextKey));
+            switchWeapon.Nodes.Add(new ScrollAxis());
+            switchWeapon.Nodes.Add(new ButtonAxis(prevGamepad, nextGamepad));
+        }
+    }
 }
